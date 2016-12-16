cda10423e3b5b54ad46be98b359a85e1c74ae486
Abhineet Tamrakar <abhineet97@users.noreply.github.com>
2016-12-16 12:57:44 +0000

    Use git instead of wget

--- remote-setup.sh
+++ remote-setup.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 #
-# Download dotfiles from Github repo.
+# Clone and copy dotfiles from Github repo to remote computer.
 
-readonly BASE_URL="https://raw.githubusercontent.com/abhineet97/dotfiles/master"
+readonly GIT_REPO="https://github.com/abhineet97/dotfiles"
 readonly FILES=(".vimrc" ".jshintrc" ".gitconfig" ".wgetrc")
 
+err() {
+  echo "$@" >&2
+  exit 1
+}
+
+if [[ -z $(which git) ]]; then
+  err "git not installed."
+fi
+
+git clone ${GIT_REPO} "${HOME}/dotfiles"
+if [[ "$?" -ne 0 ]]; then
+  err "git clone failed."
+fi
+
 for file in ${FILES[@]}; do
-  echo "${BASE_URL}/${file}"
-  curl "${BASE_URL}/${file}" > "${HOME}/${file}"
+  cp "${HOME}/dotfiles/${file}" "${HOME}/${file}"
 done
