b0fad09bd1f54742e962fee76452f3e27dcc6f6f
Abhineet Tamrakar <abhineet97@users.noreply.github.com>
2017-07-16 01:05:51 +0530

    Revert "Use symbolic links"
    This reverts commit 4cccccd2c00bd471fe631747a9e0e925a26fe38b.

--- .cmdrc.bat
+++ .cmdrc.bat
@@ -1,1 +1,17 @@
-../../.cmdrc.bat
+@echo off
+
+REM This is the file that runs automatically after cmd.exe has been launched.
+REM To stop this file from running DELETE the registry entry - HKEY_CURRENT_USER\Software\Microsoft\Command Processor\AutoRun
+
+DOSKEY LS=DIR $*
+DOSKEY RM=DEL $*
+DOSKEY CD=CD /D $*
+DOSKEY KILL=TASKKILL /IM $*
+DOSKEY POWEROFF=SHUTDOWN -S -T 00
+DOSKEY EDIT=notepad $*
+DOSKEY MEDIAMODE=set PATH=%PATH%;"C:\Program Files\MKVToolNix";"C:\Program Files\MediaInfo"
+
+CD /D %HOME%
+PROMPT $P$_$$ 
+
+@echo on
--- .vimrc
+++ .vimrc
@@ -1,1 +1,8 @@
-../../.vimrc
+set nocompatible " Be IMproved
+filetype plugin indent on
+colorscheme desert
+syntax enable                   " enable syntax processing
+set expandtab                   " use spaces for indents
+set shiftwidth=2                " no of spaces to use for indents
+set number 	                " show line numbers
+set backspace=2                 " fix backspace behaviour 
